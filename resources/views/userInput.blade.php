7bc12533c2f4f1d45cb2efc1742a23b07937cf4b
darrylrambi <darrylrambi@gmail.com>
2024-11-24 13:32:18 +0700

    added view and set route

--- resources/views/userInput.blade.php
+++ resources/views/userInput.blade.php
@@ -1,3 +1,15 @@
 <div>
     <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
+    <h1>Input data Mahasiswa</h1>
+    <form action="" method="post">
+        @csrf
+        <input type="text" id="Nama" name="Nama" placeholder="Masukan Nama Mahasiswa">
+        <br>
+        <br>
+        <input type="text" id="NIM" name="NIM" placeholder="Masukan NIM Mahasiswa">
+        <br>
+        <br>
+
+        <button>Submit</button>
+    </form>
 </div>
