af848c6cb1fa35da7bca9f67b49502b80c645119
darrylrambi <darrylrambi@gmail.com>
2024-11-24 15:09:26 +0700

    fix UI dengan menambahkan bootstrap

--- resources/views/userData.blade.php
+++ resources/views/userData.blade.php
@@ -1,20 +1,31 @@
-<div>
-    <h1>List data Mahasiswa</h1>
-    
-    <!-- show Nama -->
-    @if (!session('Nama'))
-        <h3>Nama mahasiswa tidak ada</h3>
-    @else
-        <h3>{{ session('Nama') }}</h3>
-    @endif
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List data Mahasiswa</title>
+    <link rel="stylesheet" href="{{ url('assets/css/bootstrap.min.css') }}">
+</head>
+<body>
+    <!-- Show data mahasiswa -->
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nama</th>
+                    <th scope="col">NIM</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{ session('Nama') }}</td>
+                    <td>{{ session('NIM') }}</td>
+                </tr>
+            </tbody>
+        </table>
 
-    <!-- show NIM -->
-    @if (!session('NIM'))
-        <h3>Nama mahasiswa tidak ada</h3>
-    @else
-        <h3>{{ session('NIM') }}</h3>
-    @endif
-
-    <!-- tombol back ke input data mahasiswa -->
-    <a href="{{ route('userInput') }}">Kembali</a>
-</div>
+        <!-- tombol back ke input data mahasiswa -->
+        <a href="{{ route('userInput') }}">Kembali</a>
+    </div>
+</body>
+</html>
--- resources/views/userInput.blade.php
+++ resources/views/userInput.blade.php
@@ -1,15 +1,26 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-    <h1>Input data Mahasiswa</h1>
-    <form action="userInput" method="post">
-        @csrf
-        <input type="text" id="Nama" name="Nama" placeholder="Masukan Nama Mahasiswa">
-        <br>
-        <br>
-        <input type="text" id="NIM" name="NIM" placeholder="Masukan NIM Mahasiswa">
-        <br>
-        <br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Input data Mahasiswa</title>
+    <link rel="stylesheet" href="{{ url('assets/css/bootstrap.min.css') }}">
+</head>
+<body>
+    <div class = "container">
+        <h1>Input data Mahasiswa</h1>
+        <form action="userInput" method="post">
+            @csrf
+            <div class="mb-3">
+                <input type="text" id="Nama" name="Nama" class="form-control" placeholder="Masukan Nama Mahasiswa">
+            </div>
 
-        <button>Submit</button>
-    </form>
-</div>
+            <div class="mb-3">
+                <input type="text" id="NIM" name="NIM" class="form-control" placeholder="Masukan NIM Mahasiswa">
+            </div>
+
+            <button class = "btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
